bc78c218a605e1157b6ee92a086fd72c15ddf0ed
Jonathan Souza <souzajonathan570@gmail.com>
2022-01-17 11:36:59 -0300

    first commit

---
+++ src/main/java/com/blogsapi/ProjectBlogsApiApplication.java
@@ -0,0 +1,13 @@
+package com.blogsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlogsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBlogsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogsapi/ProjectBlogsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlogsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
